3138cbc9e4066e9339aa6e2dbbfeda765ee1b120
glisselisbeth <glisselisbeth@gmail.com>
2017-06-30 14:20:13 -0500

    add class

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.get('/', (req, resp) =>{
+  resp.sendFile(__dirname+'/index.html');
+});
+
+app.use('/static', express.static(path.join(__dirname,'node_modules')));
+app.use('/static', express.static(path.join(__dirname,'assets')));
+app.listen(8080);
